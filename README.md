550c0fb38f780d34d2d335809b02bbf2b0867101
odiamond18 <odiamond@wesleyan.edu>
2022-04-26 14:54:35 -0400

    clearing all files

--- README.md
+++
@@ -1,1 +0,0 @@
-# comp333-backend
--- hw2_django/music/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Song, Rating, Year, Reg
-
-admin.site.register(Song)
-admin.site.register(Year)
-admin.site.register(Rating)
-admin.site.register(Reg)
